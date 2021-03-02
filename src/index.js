8eca266c5df12ab7055edf167f3f89ca239e9722
Dominique Ithina da Silva Oliveira <dominique.oliveira@pampa.compasso>
2021-03-02 17:50:20 -0300

    Adicionado conexão com api, retorno dos dados e knockout-validation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,112 @@
-import api from './../api/api.js'
-import css from "./style.css"
-
-// api.getCep("99700252").then((result) => {
-//  console.log(result)
-// })
+import api from './../api/api.js';
+import css from "./style.css";
 
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-    this.firstName = ko.observable("Example")
-    this.lastName = ko.observable("Example")
-    this.firstName.extend({ required: true})
+    var self = this;
+
+    //Declaração dos Observables
+
+    self.firstName = ko.observable("").extend({ required: true});
+
+    self.lastName = ko.observable("").extend({
+        required: {message: 'Preencha o sobrenome*'}
+    });
+
+    self.ddd = ko.observable("").extend({required: true});
+
+    self.phone = ko.observable("").extend({
+        required: {message: 'Preencha o telefone*'}, 
+        number: {message: 'Digite apenas numeros*'}
+    });
+
+    self.cep = ko.observable("").extend({
+        required: {message: 'Preencha o CEP*'}, 
+        number: {message: 'Digite apenas numeros*'}
+    });
+
+    self.address = ko.observable("").extend({required: true});
+
+    self.number = ko.observable("").extend({required: true});
+        
+    self.complement = ko.observable("");
+        
+    self.district = ko.observable("").extend({required: true});
+        
+    self.city = ko.observable("").extend({required: true});
+        
+    self.state = ko.observable("").extend({
+        required: true,
+        maxLength: 2
+    });
+
+    self.hasDistrict = ko.observable();
+    self.hasCity = ko.observable();
+    self.hasState = ko.observable();
+    self.hasAddress = ko.observable();
+
+    //Botao para validar o CEP
+    self.validateCep = function(){
+        
+        //Pega valores fornecidos pela API
+        api.getCep(self.cep()).then((result) => {
+            console.log(result);
+            console.log(result.erro);
+            
+            //Se o CEP inserido for retornado válido da API, preenche os dados automáticamente nos inputs
+            if(result.erro == undefined){
+                console.log("Deu certo");
+                limpaCamposCep();
+                self.address(result.logradouro);
+                self.district(result.bairro);
+                self.city(result.localidade);
+                self.state(result.uf);
+                DisableTrueOrFalse(true);
+
+            //Se não for retornado válido, limpa os inputs e habilita para preenchimento manual
+            }else{
+                console.log("Deu erro");
+                limpaCamposCep();
+                DisableTrueOrFalse(false);
+            };
+        });
+    };
+
+    self.EnviarDados = function(){
+
+        //Imprime o objeto data que recebe cada um dos valores dos inputs
+        var data = {
+            'firstName': self.firstName(),
+            'lastName' : self.lastName(),
+            'ddd': self.ddd(),
+            'phone' : self.phone(),
+            'cep': self.cep(),
+            'address' : self.address(),
+            'number' : self.number(),
+            'complement' : self.complement(),
+            'district': self.district(),
+            'city' : self.city(),
+            'state': self.state()
+        }
+        console.log(data);
+    };
+
+    //Zera os observables dos inputs para nova busca
+    function limpaCamposCep(){
+        self.address("").district("").city("").state("");
+    };
+
+    //Define valor de true ou false para travar os inputs ao buscar o CEP
+    function DisableTrueOrFalse(valor){
+        self.hasAddress(valor).hasDistrict(valor).hasCity(valor).hasState(valor);
+    };
 }
 
-const appViewModel = new AppViewModel()
+const appViewModel = new AppViewModel();
 
 // can be used in the navigation console
-window.appViewModel = appViewModel
+window.appViewModel = appViewModel;
 
 // Activates knockout.js
-ko.applyBindings(appViewModel)
+ko.validation.init();
+ko.applyBindings(appViewModel);
