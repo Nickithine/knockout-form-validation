66c0acf3ac5055c3fe8185332795f37bfb60400e
Dominique Ithina da Silva Oliveira <dominique.oliveira@pampa.compasso>
2021-03-04 15:59:23 -0300

    Mais validações, habilitar e desabilitar botão alterados

--- src/index.js
+++ src/index.js
@@ -7,18 +7,16 @@ function AppViewModel() {
 
     //Declaração dos Observables
 
-    self.todoForm = ko.observable(true);
-    self.buttonDisable = ko.observable(true);
-
-    self.buttonActive = ko.observable(false);
-
-    self.firstName = ko.observable("").extend({ required: true});
+    self.firstName = ko.observable("").extend({ 
+        required: {message: 'Preencha o nome*'}
+    });
 
     self.lastName = ko.observable("").extend({
         required: {message: 'Preencha o sobrenome*'}
     });
 
-    self.ddd = ko.observable("").extend({required: true});
+    self.ddd = ko.observable("").extend({
+        required: {message: 'Preencha o DDD*'}});
 
     self.phone = ko.observable("").extend({
         required: {message: 'Preencha o telefone*'}, 
@@ -27,95 +25,105 @@ function AppViewModel() {
 
     self.cep = ko.observable("").extend({
         required: {message: 'Preencha o CEP*'}, 
-        number: {message: 'Digite apenas numeros*'}
+        number: {message: 'Digite apenas numeros*'},
+        pattern:{
+            params: /[0-9]{5}[0-9]{3}/,
+            message: 'Digite corretamente o CEP'
+        },
+        minLength:{
+            params: 8,
+            message: 'Informe 8 digitos'
+        },
+        maxLength:{
+            params: 8,
+            message: 'Máximo de 8 digitos'
+        }
     });
 
-    self.address = ko.observable("").extend({required: true});
+    self.address = ko.observable("").extend({
+        required: {message: 'Digite o endereço*'}
+    });
 
-    self.number = ko.observable("").extend({required: true});
+    self.number = ko.observable("").extend({
+        required: {message: 'Digite o número*'}
+    });
         
-    self.complement = ko.observable("");
+    self.district = ko.observable("").extend({
+        required: {message: 'Digite o bairro*'}});
         
-    self.district = ko.observable("").extend({required: true});
-        
-    self.city = ko.observable("").extend({required: true});
+    self.city = ko.observable("").extend({
+        required: {message: 'Digite a cidade*'}});
         
     self.state = ko.observable("").extend({
-        required: true,
+        required: {message: 'Digite o UF*'},
         maxLength: 2
     });
+
+    self.buttonDisable = ko.observable(true);
+    self.complement = ko.observable("");
 
     self.hasDistrict = ko.observable();
     self.hasCity = ko.observable();
     self.hasState = ko.observable();
     self.hasAddress = ko.observable();
+    self.cepIncorreto = ko.observable(false);
 
     //Botao para validar o CEP
     self.validateCep = function(){
         
         //Pega valores fornecidos pela API
         api.getCep(self.cep()).then((result) => {
-            console.log(result);
-            console.log(result.erro);
-            
+  
             //Se o CEP inserido for retornado válido da API, preenche os dados automáticamente nos inputs
             if(result.erro == undefined){
-                console.log("Deu certo");
                 limpaCamposCep();
                 self.address(result.logradouro);
                 self.district(result.bairro);
                 self.city(result.localidade);
                 self.state(result.uf);
                 DisableTrueOrFalse(true);
+                self.cepIncorreto(false);
 
             //Se não for retornado válido, limpa os inputs e habilita para preenchimento manual
             }else{
-                console.log("Deu erro");
                 limpaCamposCep();
                 DisableTrueOrFalse(false);
+                self.cepIncorreto(true);
             };
         });
     };
-    //Verifica se os campos obrigatórios estão vazios para habilitar ou desabilitar o botao
-    function inputsVerification(){
-        if(self.firstName() && self.lastName() && self.ddd() && self.phone() && self.cep() && self.address() && self.number() && self.district() && self.city() && self.state()){
-
-            //Esconde botão desabilitado e dá show no botão ativo
-            self.buttonDisable(false);
-            self.buttonActive(true);
-        }else{
-            //Da show no botão desabilitado e esconde o botão ativo
-            self.buttonDisable(true);
-            self.buttonActive(false);
-        };
-    }
 
     //Cria um evento para sempre ouvir os movimentos dos inputs
     var inputs = document.querySelectorAll("input");
 
     inputs.forEach(function(input) {
-        input.addEventListener("mouseleave", function() {
-            inputsVerification();
+        input.addEventListener(("blur", "keyup"), function() {
+            verificaInputs();
         });
     });
 
+    //Botão de Enviar
     self.EnviarDados = function(){
-
+        //Faz uma validação se os campos estão preenchidos corretamente e 
         //Imprime o objeto data que recebe cada um dos valores dos inputs
-        var data = {
-            'firstName': self.firstName(),
-            'lastName' : self.lastName(),
-            'ddd': self.ddd(),
-            'phone' : self.phone(),
-            'cep': self.cep(),
-            'address' : self.address(),
-            'number' : self.number(),
-            'complement' : self.complement(),
-            'district': self.district(),
-            'city' : self.city(),
-            'state': self.state()
+        if (appViewModel.errors().length === 0) {
+            var data = {
+                'firstName': self.firstName(),
+                'lastName' : self.lastName(),
+                'ddd': self.ddd(),
+                'phone' : self.phone(),
+                'cep': self.cep(),
+                'address' : self.address(),
+                'number' : self.number(),
+                'complement' : self.complement(),
+                'district': self.district(),
+                'city' : self.city(),
+                'state': self.state()
+            }
+            console.log(data);
+        }else{
+            alert('Preencha corretamente o formulário!')
         }
-        console.log(data);
     };
 
     //Zera os observables dos inputs para nova busca
@@ -127,6 +135,19 @@ function AppViewModel() {
     function DisableTrueOrFalse(valor){
         self.hasAddress(valor).hasDistrict(valor).hasCity(valor).hasState(valor);
     };
+
+    //Verifica se existem erros no validations e retorna botão habilitado ou não
+    function verificaInputs() {
+        if (appViewModel.errors().length === 0) {
+            console.log('Obrigado');
+            self.buttonDisable(false);
+        }
+        else {
+            appViewModel.errors.showAllMessages();
+            console.log('Preencha o formulário corretamente');
+            self.buttonDisable(true);
+        }
+    }
 }
 
 const appViewModel = new AppViewModel();
@@ -135,5 +156,5 @@ const appViewModel = new AppViewModel();
 window.appViewModel = appViewModel;
 
 // Activates knockout.js
-ko.validation.init();
 ko.applyBindings(appViewModel);
+appViewModel.errors = ko.validation.group(appViewModel);
